819951bce27b499034ee99e67cc34523c787f99f
Sri Sairam Pothuri <sairamp98@Sris-MacBook-Pro.local>
2024-07-22 16:39:08 -0500

    created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Signin from './pages/Signin'
+import Profile from './pages/Profile'
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-up' element={<Signup/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+
+  </BrowserRouter>
 }
