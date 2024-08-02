e2b5fab6e14cd9312ba49684455b15e0d0eff512
sairamp98 <sri.sai.ram.pothuri-1@ou.edu>
2024-08-02 14:31:58 -0500

    created signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/usermodel.js";
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) =>{
+    const {username, email, password} = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    try{
+        const newUser = await User.create({
+            username: username,
+            email: email,
+            password: hashedPassword
+        });
+        res.status(201).json({
+            msg: "User created successfully"
+        })
+    }
+    catch(e) {
+        res.json({msg: e.message});
+    }
+}
--- api/index.js
+++ api/index.js
@@ -2,14 +2,17 @@ import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server listening to port 3000!");
 })
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes)
