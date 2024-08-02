e181479d85f4d65ce0b80c1ae6b5a8871c89bbd9
sairamp98 <sri.sai.ram.pothuri-1@ou.edu>
2024-08-02 14:49:43 -0500

    added a middleware and a function to handle custom errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/usermodel.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) =>{
+export const signup = async (req, res, next) =>{
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     try{
@@ -15,6 +15,6 @@ export const signup = async (req, res) =>{
         })
     }
     catch(e) {
-        res.json({msg: e.message});
+        next(e);
     }
 }
