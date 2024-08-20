66b22dce45016242bf3ef35883f176f60ab7e1a3
sairamp98 <sri.sai.ram.pothuri-1@ou.edu>
2024-08-20 17:58:42 -0500

    created an update user info api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,30 @@
+import User from "../models/usermodel.js";
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) => {
     res.json({
         message: "API is working",
     });
+}
+export const updateUser = async (req, res, next) => {
+    if(req.user.id != req.params.id) {
+        return next(errorHandler(401, "You can update only your account!"))
+    }
+    try {
+        if(req.body.password) {
+            req.body.password = await bcrypt.hash(req.body.password, 12);
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set : {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                profilePicture: req.body.profilePicture
+            }
+        }, {new: true});
+        const {password, ...rest} = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 }
